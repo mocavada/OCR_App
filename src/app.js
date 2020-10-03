b37230e9ad0fa0baca5704263ad9e124b4181a74
mocavada <marc@awesomewebsite.ca>
2020-10-03 02:18:00 -0400

    OCR success 2

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const app = express();
 const fs = require("fs");
 const multer = require("multer");
 const Tesseract = require("tesseract.js");
-const worker = Tesseract.createWorker();
+const { createWorker } = require("tesseract.js");
+const { text } = require("body-parser");
+const worker = createWorker({
+    loader: m => console.log(m)
+});
 
-
-  
 // Declare Storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -23,7 +25,6 @@ const upload = multer({storage: storage}).single("avatar");
 // View
 app.set("view engine", "ejs");
 
-
 // Routes
 app.get("/", (req, res) => {
      res.render('index');
@@ -34,19 +35,29 @@ app.post("/upload", (req, res) => {
         fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
             if(err) return console.log('This is the error', err);
             Tesseract
-            .recognize(data, 'eng', { 
-                logger: m => console.log(m), 
-                tessjs_create_pdf: '1'}).then(result => {
-                    res.send(result.data.text);
-                }).finally(() => 
-                worker.terminate());
+                .recognize(data, 'eng', { 
+                    logger: m => console.log(m), 
+                    })
+                .then(result => {
+                    const text = res.send(result.data.text);
+                   }).finally(() => {
+                    worker.terminate();
+                    console.log("Worker has been terminated")});
         });
+
+        
     });
 });
 
+// app.get('/downloads', (req, res) => {
+//     const file = `${__dirname}/`
+// })
+
 
 const PORT = 5000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Yo Running in port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Yo Running in port ${PORT}`)
+});
 
 
 
